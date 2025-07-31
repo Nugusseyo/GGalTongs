4b53366cb4dc262ae30aff580bfdd6d98ad2b95d
jjoung0911 <chjw091118@gmail.com>
2025-07-31 23:16:25 +0900

    feat : ScanSkill

---
+++ Assets/JW/01.Scripts/Skill/ScanEffect.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using UnityEngine;
+using DG.Tweening;
+using UnityEditor;
+
+public class ScanEffect : MonoBehaviour
+{
+    public static ScanEffect Instance { get; private set; }
+
+    private void Awake()
+    {
+        DOTween.Init(false,true,LogBehaviour.Verbose).SetCapacity(200, 50);
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+    }
+
+    private void Start()
+    {
+        transform.DOScale(3f, 0.5f).OnComplete((() =>
+        {
+            transform.DOScale(0, 0.5f); 
+            Destroy(gameObject, 0.5f);
+        }));
+    }
+    
+    public void Follow(GameObject enemy)
+    {
+        transform.position = enemy.transform.position;
+        Debug.Log(enemy);
+    }
+}
