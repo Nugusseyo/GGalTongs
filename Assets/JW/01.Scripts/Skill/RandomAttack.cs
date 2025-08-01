37a11ab0eafad86a85376217098f1204b0fd87f4
Nugusaeyo <chsj0702@gmail.com>
2025-08-01 19:29:54 +0900

    clone code deleted

--- Assets/JW/01.Scripts/Skill/RandomAttack.cs
+++ Assets/JW/01.Scripts/Skill/RandomAttack.cs
@@ -28,13 +28,6 @@ public class RandomAttack : SkillSO
         Debug.Log("Reset");
     }
 
-    protected override void ResetSkillData()
-    {
-        base.ResetSkillData();
-        ActiveCount = CurrentLevel;
-        Debug.Log("Reset");
-    }
-
     protected override void Active()
     {
         for (int i = 0; i < ActiveCount; i++)
