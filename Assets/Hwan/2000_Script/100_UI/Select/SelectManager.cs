de7c5c4387a8dcb787616ab15d01ad056a84905f
Hwanji203 <hwanji203@gmail.com>
2025-08-01 22:06:37 +0900

    Prefab_

--- Assets/Hwan/2000_Script/100_UI/Select/SelectManager.cs
+++ Assets/Hwan/2000_Script/100_UI/Select/SelectManager.cs
@@ -16,7 +16,6 @@ public class SelectManager : MonoBehaviour
         if (Instance == null)
         {
             Instance = this;
-            DontDestroyOnLoad(gameObject);
         }
         else
         {
