4515ab0a35d103e37d36fa7ba4f1b9522b942721
Hwanji203 <hwanji203@gmail.com>
2025-08-02 14:11:39 +0900

    End

--- Assets/BBJ/01.Script/TitleGUI.cs
+++ Assets/BBJ/01.Script/TitleGUI.cs
@@ -4,13 +4,19 @@ using UnityEngine.SceneManagement;
 public class TitleGUI : MonoBehaviour
 {
     [SerializeField] private GameObject settingGUI;
+
+    private void Awake()
+    {
+        settingGUI.SetActive(false);
+    }
+
     public void OnEnd()
     {
         Application.Quit();
     }
     public void OnSettingUI()
     {
-        settingGUI.SetActive(true);
+        settingGUI.SetActive(!settingGUI.activeSelf);
     }
     public void StartIngame()
     {
